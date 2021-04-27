dd6a3fdb32e2c014bd8fe39bc857d5b19d697c09
Alexander <snagovskiy@users.noreply.github.com>
2021-04-27 10:33:38 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,6 +109,23 @@ $(window).scroll(function() {
   };
 });
 
+// Add it after jquery.magnific-popup.js and before first initialization code
+$.extend(true, $.magnificPopup.defaults, {
+  tClose: 'Закрыть (Esc)', // Alt text on close button
+  tLoading: 'Загрузка...', // Text that is displayed during loading. Can contain %curr% and %total% keys
+  gallery: {
+    tPrev: 'Предыдущий (Left arrow key)', // Alt text on left arrow
+    tNext: 'Следущий (Right arrow key)', // Alt text on right arrow
+    tCounter: '%curr% of %total%' // Markup for "1 of 7" counter
+  },
+  image: {
+    tError: '<a href="%url%">Изображение</a> не может быть загруженно.' // Error message when image could not be loaded
+  },
+  ajax: {
+    tError: '<a href="%url%">Изображение</a> не может быть загруженно.' // Error message when ajax request failed
+  }
+});
+
 // Magnific Popup
 
 $(document).ready(function() {
