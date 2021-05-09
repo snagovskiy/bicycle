e614e3ff52d86a3bc167cfc971febed345707835
Alexander <snagovskiy@users.noreply.github.com>
2021-05-09 13:52:41 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,12 +170,6 @@ $(document).ready(function() {
 		closeOnContentClick: false,
 		closeBtnInside: false,
 		mainClass: 'mfp-with-zoom mfp-img-mobile',
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
-			}
-		},
 		gallery: {
 			enabled: true
 		},
